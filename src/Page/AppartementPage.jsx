b6fb3ba7247e0cb505236c96331f4ab6b29eeb56
Fanny Hadji <devfanny91@outlook.com>
2024-01-11 22:17:16 +0100

    Responsif page logement + sass

--- src/Page/AppartementPage.jsx
+++ src/Page/AppartementPage.jsx
@@ -1,45 +1,33 @@
 import React from 'react'
 import "./AppartementPage.scss";
+import { AppartementBanner } from "../components/AppartementBanner";
+import { AppartementHeader } from '../components/AppartementHeader';
+
 
 function AppartementPage() {
-    return <div className="Appartement-page">
-        <div>
-            <img src="public/Background.png" alt="" />
-        </div>
-        <div className="appartement__header">
-            <div className="appartement__container">
-                <h1>Crazy loft on the canal Saint-Martin</h1>
-                <h2>Paris, Ile de France</h2>
-                <div className="appartement__tags">
-                    <span>Cosy</span>
-                    <span>Canal</span>
-                    <span>Paris 10</span>
+    return (
+        <div className="Appartement-page">
+            <AppartementBanner />
+            <AppartementHeader />
+            <div className="appartement__collapse">
+                <div className="appartement__description">
+                    <p className="description__header">
+                        <span>Description</span>
+                        <img src="public/vectorHaut.svg"></img>
+
+                    </p>
+                    <p className="description__content">kjirv,opjopjiouhughuhiohknikjijijpinkpok!</p>
                 </div>
-            </div>
-            <h3>
-                <splan>Alaxandre </splan>
-                <splan>Dumas </splan>
-            </h3>
-            <div className="appartement__client__badge">
-                <img src="public/Host.png" alt="badge" />
-                <div className="appartement__client__stars">
-                    <span> ☆</span>
-                    <span> ☆</span>
-                    <span> ☆</span>
-                    <span> ☆</span>
-                    <span> ☆</span>
+                <div className="appartement__equipment">
+                    <p className="description__header">
+                        <span>Equipements</span>
+                        <img src="public/vectorBas.svg"></img>
+                    </p>
+                    <p className="description__content">ggggggggggggggggggggggggggggggggg</p>
                 </div>
-            </div>
+            </div >
         </div>
-        <div className="appartement__description">
-            <p>Description</p>
-            <p>kjirv,opjopjiouhughuhiohknikjijijpinkpok!</p>
-        </div>
-        <div className="appartement__equipement">
-            <p>Equipements</p>
-            <p>ggggggggggggggggggggggggggggggggggggg</p>
-        </div>
-    </div>
+    )
 }
 
 export default AppartementPage
---
+++ src/components/AppartementBanner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./AppartementBanner.scss";
+
+export function AppartementBanner() {
+    return (
+        <div className="appartement__banner">
+            <img src="public/Background.png" alt="" />
+        </div>
+    );
+}
+
+
